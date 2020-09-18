164fbbf205b5c72ecf282c7d7693eab332d32ef3
Alex Roan <alex.roan@hotmail.com>
2020-09-18 23:49:08 +0100

    Update deployment script

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,11 +1,11 @@
-const TestOracle = artifacts.require("TestOracle");
-// const USM = artifacts.require("USM");
+const ChainlinkOracle = artifacts.require("ChainlinkOracle");
+const ExampleEthBackedToken = artifacts.require("ExampleEthBackedToken");
 
 module.exports = async function(deployer) {
     // For some reason, this helps `oracle.address`
     // not be undefined??
     await web3.eth.net.getId();
     
-    let oracle = await deployer.deploy(TestOracle);
-    // await deployer.deploy(USM, oracle.address);
+    let oracle = await deployer.deploy(ChainlinkOracle);
+    await deployer.deploy(ExampleEthBackedToken, oracle.address);
 }
